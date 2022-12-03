4600d4b9cb103e7a71e25d42bf9e529b1a8e681f
christarot <114644682+christarot@users.noreply.github.com>
2022-12-03 12:03:07 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,12 +161,13 @@
 					if (browser.mobile) {
 
 						$reel
-							.css('overflow-y', 'hidden')
+							//.css('overflow-y', 'hidden')
 							//.css('overflow-x', 'visible')
 							.css('overflow-x', 'scroll')
 							//.css('overflow-y', 'visible')
 							//.css('overflow-x', 'scroll')
 							//.css('overflow', 'visible')
+							.css('overflow', 'hidden')
 
 							.scrollLeft(0);
 						
